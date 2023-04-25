f9730fce355714d0aa307115c371e9700f260c1a
EstrellaXD <estrellaxd05@gmail.com>
2023-04-25 23:11:08 +0800

    Fix torrent

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi.responses import Response, FileResponse
 
-from server import get_rss_proxy, PORT
+from server import get_rss_proxy,get_file_proxy, PORT
 
 app = FastAPI()
 
@@ -10,6 +10,14 @@ app = FastAPI()
 async def get_rss(token: str):
     content = get_rss_proxy(token)
     return Response(content=content, media_type="application/xml")
+
+
+@app.get("/Download/{file_path:path}")
+async def get_file(file_path: str):
+    print(file_path)
+    content = get_file_proxy(file_path)
+    # return torrent file
+    return Response(content=content, media_type="application/x-bittorrent")
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -8,6 +8,12 @@ load_dotenv(".env")
 URL = os.getenv("URL")
 PORT = os.getenv("PORT") if os.getenv("PORT") else 6789
 
+
 def get_rss_proxy(token):
     req = requests.get(f"https://mikanani.me/RSS/MyBangumi?token={token}")
     return re.sub(r"https://mikanani.me", f"https://{URL}", req.text)
+
+
+def get_file_proxy(file_path):
+    req = requests.get(f"https://mikanani.me/Download/{file_path}")
+    return req.content
